0987874593b1aefcc983fb7652a1a5c360514ddf
joaoDevL <joao.software.dev@gmail.com>
2026-01-23 14:17:16 -0500

    feat: Adding the dto for the controller

---
+++ src/main/java/com/joao/dev/clinica_odontologica/dto/GlobalResponse.java
@@ -0,0 +1,35 @@
+package com.joao.dev.clinica_odontologica.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class GlobalResponse<T> {
+    private Boolean ok;
+    private String message;
+    private T data;
+    @Builder.Default
+    private LocalDateTime timestamp = LocalDateTime.now();
+    private String details;
+
+    public static <T> GlobalResponse<T> success(T data, String message) {
+        return GlobalResponse.<T>builder()
+                .ok(true)
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> GlobalResponse<T> failure(String message, String details) {
+        return GlobalResponse.<T>builder()
+                .ok(false)
+                .message(message)
+                .details(details)
+                .build();
+    }
+}
