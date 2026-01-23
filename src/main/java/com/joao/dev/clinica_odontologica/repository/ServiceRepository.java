a16c9d9152d72010d0f9b51764b5b277f3a69a79
joaoDevL <joao.software.dev@gmail.com>
2026-01-23 14:10:28 -0500

    feat: Adding the repository for the Service entity

---
+++ src/main/java/com/joao/dev/clinica_odontologica/repository/ServiceRepository.java
@@ -0,0 +1,12 @@
+package com.joao.dev.clinica_odontologica.repository;
+
+import com.joao.dev.clinica_odontologica.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+    List<Service> findByIsActiveTrue();
+}
